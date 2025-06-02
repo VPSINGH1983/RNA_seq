59f0bdf5af10092a363e99e566870a9d49c5b6d7
VPSINGH1983 <singhvijay83@gmail.com>
2025-06-02 12:50:55 +0530

    Git

---
+++ git.R
@@ -0,0 +1,12 @@
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "VPSINGH1983", user.email = "singhvijay83@gmail.com")
+use_git()
+create_github_token()
+
+library(gitcreds)
+gitcreds_set()
+
+library(usethis)
+use_github()
